c9b5e77e557eb8e14648802f07295d6e92d9a1fd
Nikita Loskutov <cnaize@gmail.com>
2019-03-27 22:00:13 +0400

    fixed get user method

--- db/sqlite/user.go
+++ db/sqlite/user.go
@@ -3,6 +3,7 @@ package sqlite
 import (
 	"github.com/cnaize/mz-common/model"
 	"github.com/jinzhu/gorm"
+	"strings"
 )
 
 // TODO:
@@ -11,7 +12,7 @@ import (
 // WARNING: not thread safe
 func (db *DB) GetUser(user model.User) (model.User, error) {
 	var res model.User
-	if err := db.db.First(&res, "username = ?", user.Username).Error; err != nil {
+	if err := db.db.First(&res, "LOWER(username) = ?", strings.ToLower(user.Username)).Error; err != nil {
 		return res, err
 	}
 
